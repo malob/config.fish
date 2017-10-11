937e315db10e0b2b57453cd822579669c2972b69
Malo Bourgon <mbourgon@gmail.com>
2017-10-11 16:50:40 -0700

    Tidying up config.fish

--- config.fish
+++ config.fish
@@ -1,6 +1,12 @@
+# Aliases
 alias cat="ccat"
 alias airpods="osascript /Users/malo/Library/Mobile\ Documents/com~apple~ScriptEditor2/Documents/Connect\ AirPods.scpt"
+alias beats="osascript /Users/malo/Library/Mobile\ Documents/com~apple~ScriptEditor2/Documents/Connect\ Beats.scpt"
+
+# Theme settings
 set -gx theme_nerd_fonts yes
 set -gx theme_color_scheme solarized-dark
+
+# Other
 set -gx EDITOR vim
 set -gx fish_key_bindings fish_vi_key_bindings
