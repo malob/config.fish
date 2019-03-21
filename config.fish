2291ba793f68f2bbb49e076d653acb199eca91f2
Malo Bourgon <mbourgon@gmail.com>
2019-03-21 10:56:54 -0700

    Small tweaks and plugin update

--- config.fish
+++ config.fish
@@ -3,7 +3,6 @@ alias ghci="stack ghci"
 alias ghc="stack ghc"
 alias cat="bat"
 alias ping="prettyping"
-alias preview="fzf --preview 'bat --color \"always\" {}'"
 
 # Theme settings
 set -gx theme_nerd_fonts yes
@@ -11,16 +10,4 @@ set -gx theme_color_scheme solarized-dark
 
 # Other
 set -gx EDITOR nvr -o
-set -gx fish_key_bindings fish_vi_key_bindings
 set -gx fish_term24bit 1
-
-# tabtab source for serverless package
-# uninstall by removing these lines or running `tabtab uninstall serverless`
-[ -f /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.fish ]; and . /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.fish
-# tabtab source for sls package
-# uninstall by removing these lines or running `tabtab uninstall sls`
-[ -f /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.fish ]; and . /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.fish
-
-# tabtab source for slss package
-# uninstall by removing these lines or running `tabtab uninstall slss`
-[ -f /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/slss.fish ]; and . /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/slss.fish
