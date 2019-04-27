a803e4a30d74dfe6727d3c2ed99f61bc3529a5b0
Malo Bourgon <mbourgon@gmail.com>
2019-04-27 11:42:21 -0700

    Exec fenv only on Darwin

--- conf.d/00-nix.fish
+++ conf.d/00-nix.fish
@@ -1,1 +1,3 @@
-fenv source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+if test (nix eval nixpkgs.stdenv.isDarwin) = 'true'
+  fenv source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+end
