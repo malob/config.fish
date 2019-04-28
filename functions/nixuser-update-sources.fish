d8ccb2e0204417ab72ced89c2b37fa46f3d277a7
Malo Bourgon <mbourgon@gmail.com>
2019-04-28 16:18:11 -0700

    Add updating nix custom python package sources

--- functions/nixuser-update-sources.fish
+++ functions/nixuser-update-sources.fish
@@ -1,11 +1,15 @@
 function nixuser-update-sources
   nix-channel --update
   pushd ~/.config/nixpkgs/pkgs/node-packages
-  echo "Updating Node packages ..."
+  echo \n"Updating Node package nix expressions ..."\n
   node2nix --nodejs-10 -i node-packages.json
   popd
   pushd ~/.config/nixpkgs/pkgs/ruby-gems/
-  echo "Updating Ruby Gems ..."
-  bundix --magic > /dev/null
+  echo \n"Updating Ruby Gems nix expressions ..."\n
+  bundix --magic
+  popd
+  pushd ~/.config/nixpkgs/pkgs/python-packages
+  echo \n"Updating Python package nix expressions ..."\n
+  pypi2nix --python-version 3.6 --requirements requirements.txt
   popd
 end
