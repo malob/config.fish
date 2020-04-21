5c20990af29772db1ff1d89a91b32fa7b4a98a81
Malo Bourgon <mbourgon@gmail.com>
2020-04-21 10:31:16 -0700

    Fix test of whether fish is running on NixOS

--- conf.d/00-nix.fish
+++ conf.d/00-nix.fish
@@ -1,4 +1,4 @@
-if test ! -f /nix/NIXOS
+if test ! -e /etc/NIXOS
   set -pg fish_function_path ~/.nix-profile/share/fish-foreign-env/functions
   fenv source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 end
