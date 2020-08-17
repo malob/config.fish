2d33a10ecc99416e9efd29de698440a9f6a9ed47
Malo Bourgon <mbourgon@gmail.com>
2020-08-17 13:02:08 -0700

    Update nix enviroment setup

--- conf.d/00-nix.fish
+++ conf.d/00-nix.fish
@@ -1,4 +1,8 @@
 if test ! -e /etc/NIXOS
   set -pg fish_function_path ~/.nix-profile/share/fish-foreign-env/functions
-  fenv source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+  fenv source ~/.nix-profile/etc/profile.d/nix-daemon.sh
+end
+
+if test -e /nix
+   set -pgx NIX_PATH nixpkgs=$HOME/.nix-defexpr/nixpkgs.nix
 end
